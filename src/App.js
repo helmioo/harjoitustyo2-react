50a0136fe7e1d6de45dfe98d78ab89bbfed4e1e5
helmioo <81010386+helmioo@users.noreply.github.com>
2021-10-04 17:22:34 +0300

    toimimaton versio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+const URL= 'https://www.boredapi.com/api/activity'
 
 function App() {
+
+  const [activity, setActivity] = useState(0)
+  const [participants, setParticipants] = useState(0)
+  const [result, setResult] = useState('')
+
+  async function search(e) {
+    e.preventDefault()
+
+    try {
+      const address = URL + '?type=' + activity + '&participants=' + participants
+      const response = await fetch(address)
+  
+      if (response.ok) {
+      const json = await response.json()
+      console.log(json.activity)
+      setResult(json.activity)
+      }
+      else {
+        alert('Error retrieving activity.')
+      }
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Get an activity</h3>
+      <form onSubmit={search}>
+      <div>
+        <label>Select an activity category:</label>
+      </div>
+      <div>
+        <select name="activity" value={activity} onChange={e=> setActivity(e.target.value)}>
+          <option value="education">Education</option>
+          <option value="recreational">Recreational</option>
+          <option value="social">Social</option>
+          <option value="diy">Diy</option>
+          <option value="charity">Charity</option>
+          <option value="cooking">Cooking</option>
+          <option value="relaxation">Relaxation</option>
+          <option value="music">Music</option>
+          <option value="busywork">Busywork</option>
+        </select>
+      </div>
+      <div>
+        <label>Number of participants:</label>
+      </div>
+      <div>
+        <input type="number" onChange={e=> setParticipants(e.target.value)}></input>
+      </div>
+      <div>
+      <button>Get activity</button>
+      </div>
+      <div>
+        <output>{result}</output>
+      </div>
+    </form>
     </div>
   );
 }
