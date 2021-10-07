9bdef7f20064651cb038001100f782e4794c675c
helmioo <81010386+helmioo@users.noreply.github.com>
2021-10-07 18:44:45 +0300

    lisätty virheenilmoitus

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react'
 
-const URL= 'https://www.boredapi.com/api/activity'
+const URL = 'https://www.boredapi.com/api/activity'
 
 function App() {
 
@@ -16,20 +16,26 @@ function App() {
       setResult('Looking for an activity, please wait.')
       const address = URL + '?type=' + activity + '&participants=' + participants
       const response = await fetch(address)
-  
+
       if (response.ok) {
-      const json = await response.json()
-      console.log(json)
-      setResult(json.activity)
+        const json = await response.json()
+        console.log(json)
+
+        if (json.error) {
+          setResult('Could not find activity with given options.')
+        } else {
+          setResult(json.activity)
+        }
+
       }
       else {
         setResult('')
-        console.log(alert);
+        console.log();
         alert('Error retrieving activity.')
       }
     } catch (err) {
-      console.log(setResult);
-      setResult('')
+      console.log();
+      setResult('error')
       alert(err)
     }
   }
@@ -38,35 +44,35 @@ function App() {
     <div>
       <h3>Get an activity</h3>
       <form onSubmit={search}>
-      <div>
-        <label>Select an activity category:</label>
-      </div>
-      <div>
-        <select name="activity" value={activity} onChange={e=> setActivity(e.target.value)}>
-          <option value="education">Education</option>
-          <option value="recreational">Recreational</option>
-          <option value="social">Social</option>
-          <option value="diy">Diy</option>
-          <option value="charity">Charity</option>
-          <option value="cooking">Cooking</option>
-          <option value="relaxation">Relaxation</option>
-          <option value="music">Music</option>
-          <option value="busywork">Busywork</option>
-        </select>
-      </div>
-      <div>
-        <label>Number of participants:</label>
-      </div>
-      <div>
-        <input type="number" onChange={e=> setParticipants(e.target.value)}></input>
-      </div>
-      <div>
-      <button>Get activity</button>
-      </div>
-      <div>
-        <output>{result}</output>
-      </div>
-    </form>
+        <div>
+          <label>Select an activity category:</label>
+        </div>
+        <div>
+          <select name="activity" value={activity} onChange={e => setActivity(e.target.value)}>
+            <option value="education">Education</option>
+            <option value="recreational">Recreational</option>
+            <option value="social">Social</option>
+            <option value="diy">Diy</option>
+            <option value="charity">Charity</option>
+            <option value="cooking">Cooking</option>
+            <option value="relaxation">Relaxation</option>
+            <option value="music">Music</option>
+            <option value="busywork">Busywork</option>
+          </select>
+        </div>
+        <div>
+          <label>Number of participants:</label>
+        </div>
+        <div>
+          <input type="number" onChange={e => setParticipants(e.target.value)}></input>
+        </div>
+        <div>
+          <button>Get activity</button>
+        </div>
+        <div>
+          <output>{result}</output>
+        </div>
+      </form>
     </div>
   );
 }
